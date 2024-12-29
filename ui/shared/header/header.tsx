50ef6bf08cf8226c89c806d44ccd8a05ba07ef6c
billalxcode <billal.xcode@gmail.com>
2024-12-29 07:30:24 +0700

    add padding

--- ui/shared/header/header.tsx
+++ ui/shared/header/header.tsx
@@ -3,7 +3,7 @@ import HeaderNetworkSelect from "./network";
 
 export default function Header() {
     return (
-        <div className='flex justify-between items-center w-full p-4 select-none'>
+        <div className='flex justify-between items-center w-full p-4 select-none px-8'>
             <p className='font-bold'>DREYERX EXPLORER</p>
             
             <HeaderMenu />
