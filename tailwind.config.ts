45735c25ab92c1b4227a2661ca34fae64d2eccc3
billalxcode <billal.xcode@gmail.com>
2024-12-29 07:29:31 +0700

    add container color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
 
+        container: {
+          default: "rgba(255, 255, 255, 0.1)",
+          hover: "rgba(255, 255, 255, 0.2)",
+        },
+        effect: {
+          cursor: {
+            default: "rgba(195, 0, 225, 0.35)",
+          }
+        },
         icon: {
           default: "rgba(255, 255, 255, 0.5)",
           hover: 'rgba(255, 255, 255, 0.8)',
