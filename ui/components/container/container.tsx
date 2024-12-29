3ce7fb21ca9e2cbb53a0de7951432a521efbd65f
billalxcode <billal.xcode@gmail.com>
2024-12-29 07:30:06 +0700

    add container

---
+++ ui/components/container/container.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Container(props: {
+    children: React.ReactNode
+}) {
+  return (
+    <div className="min-h-screen w-screen px-8 py-8">
+      {props.children}
+    </div>
+  )
+}
