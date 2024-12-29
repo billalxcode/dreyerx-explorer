bdf444791e8b3097974cd0e60af27f558997ceb1
billalxcode <billal.xcode@gmail.com>
2024-12-29 07:30:00 +0700

    add new networks

--- config/constants.ts
+++ config/constants.ts
@@ -1,13 +1,18 @@
 export type ExplorerNetworkTypes = {
+    name: string;
     baseURL: string;
     apiURL: string;
-    name: string;
 };
 
 export const explorer_networks: ExplorerNetworkTypes[] = [
     {
+        name: "Mainnet",
         baseURL: "http://localhost:3000",
-        apiURL: "https://testnet-scan.dreyerx.com/api",
+        apiURL: "https://scan.dreyerx.com/api",
+    },
+    {
         name: "Testnet",
+        baseURL: "http://localhost:3001",
+        apiURL: "https://testnet-scan.dreyerx.com/api",
     }
 ]
