f22448d53b8dff649d7e71e0fd591fd2a1311316
billalxcode <billal.xcode@gmail.com>
2024-12-30 08:48:41 +0700

    make container as a flex

--- ui/components/container/container.tsx
+++ ui/components/container/container.tsx
@@ -4,7 +4,7 @@ export default function Container(props: {
     children: React.ReactNode
 }) {
   return (
-    <div className="min-h-screen w-screen px-8 py-8">
+    <div className="min-h-screen w-screen px-8 py-8 flex flex-col gap-4">
       {props.children}
     </div>
   )
