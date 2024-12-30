7b0867267e39fce82449e29dc75ac427f22f80cc
billalxcode <billal.xcode@gmail.com>
2024-12-30 08:49:31 +0700

    add latest transactions

---
+++ ui/shared/home/latest/transactions.tsx
@@ -0,0 +1,12 @@
+import Card from '@/ui/components/card/card'
+import React from 'react'
+
+export default function LatestTransactions() {
+  return (
+    <Card title='Latest Transactions' className='w-full'>
+        <div className="flex flex-col w-full h-full justify-center items-center">
+            <p>No Data</p>
+        </div>
+    </Card>
+  )
+}
