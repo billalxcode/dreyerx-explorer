0f7970c3fb5342a8f42fb03a6aa1d7f03769356d
billalxcode <billal.xcode@gmail.com>
2024-12-30 17:20:58 +0700

    add new components skeleton for text

---
+++ ui/components/skeleton/text.tsx
@@ -0,0 +1,9 @@
+export default function SkeletonText(props: {
+    className?: string
+}) {
+    return (
+        <div className={`animate-pulse ${props.className}`}>
+            <div className="bg-white/20 h-5 w-1/2 rounded-lg"></div>
+        </div>
+    );
+}
