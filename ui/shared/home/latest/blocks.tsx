7cf44db5da12aa341d1c0f23a28a05e6b29116b4
billalxcode <billal.xcode@gmail.com>
2024-12-30 08:49:23 +0700

    add latest block widgets

---
+++ ui/shared/home/latest/blocks.tsx
@@ -0,0 +1,12 @@
+import Card from '@/ui/components/card/card'
+import React from 'react'
+
+export default function LatestBlocks() {
+  return (
+    <Card title='Latest Blocks' className='w-1/4'>
+        <div className="flex flex-col w-full h-full justify-center items-center">
+            <p>No Data</p>
+        </div>
+    </Card>
+  )
+}
