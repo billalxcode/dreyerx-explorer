161704da2b39cd79699e734b40f53f1af0d15763
billalxcode <billal.xcode@gmail.com>
2024-12-30 17:21:15 +0700

    add skeleton text to hero item

--- ui/shared/home/item.tsx
+++ ui/shared/home/item.tsx
@@ -1,14 +1,22 @@
 import Card from "@/ui/components/card/card";
+import SkeletonText from "@/ui/components/skeleton/text";
 import React from "react";
 
 export default function HeroItem(props: {
     title: string,
-    value: string
+    value: string,
+    isLoading?: boolean
 }) {
   return (
     <Card className="w-full">
       <h2 className="text-white/50 text-sm">{ props.title }</h2>
-      <p className="font-semibold text-lg">{ props.value }</p>
+      {
+        props.isLoading? (
+          <SkeletonText />
+        ) : (
+          <p className="font-semibold text-lg">{ props.value }</p>
+        )
+      }
     </Card>
   );
 }
