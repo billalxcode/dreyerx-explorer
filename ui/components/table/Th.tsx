ea6100791efb0b6b5040b1b7c54f71131fbe3b9c
billalxcode <billal.xcode@gmail.com>
2024-12-31 07:41:53 +0700

    add component table header

---
+++ ui/components/table/Th.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TableHeaderCol(props: {
+  children: React.ReactNode;
+}) {
+  return (
+    <th className={`px-4 py-2 border border-container-default uppercase`}>
+      {props.children}
+    </th>
+  );
+}
