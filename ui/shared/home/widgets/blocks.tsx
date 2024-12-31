b61ae8a39eb20d3ebba56cd9a2f35e6965c15023
billalxcode <billal.xcode@gmail.com>
2024-12-31 09:52:03 +0700

    update blocks

--- ui/shared/home/widgets/blocks.tsx
+++ ui/shared/home/widgets/blocks.tsx
@@ -3,6 +3,7 @@ import Card from '@/ui/components/card/card';
 import React, { useEffect, useRef } from 'react';
 import BlocksItem from './blocks-item';
 import { motion } from 'motion/react';
+import Link from 'next/link';
 
 export default function LatestBlocks() {
     const timerRef = useRef<NodeJS.Timeout | null>(null);
@@ -23,7 +24,15 @@ export default function LatestBlocks() {
     }, [handleFetchBlocks]);
 
     return (
-        <Card title="Latest Blocks" className="w-1/3">
+        <Card
+            title="Latest Blocks"
+            className="w-1/3"
+            toolbar={
+                <Link href={'/blocks'}>
+                    <h3 className="text-sm font-semibold">View More</h3>
+                </Link>
+            }
+        >
             <div className="flex flex-col gap-2 mt-2">
                 {blocks?.map((block, index) => {
                     if (index == 0) {
