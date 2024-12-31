e239c3074cb5cf6a66b90122fad4b4f89b3727ad
billalxcode <billal.xcode@gmail.com>
2024-12-31 07:41:19 +0700

    add table container

---
+++ ui/components/table/container.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function TableContainer(props: {
+    children: React.ReactNode;
+}) {
+  return (
+    <div className="overflow-x-auto mt-2">
+        <table className="table-auto w-full border-collapse border rounded bg-container-default text-sm">
+            { props.children}
+        </table>
+    </div>
+  );
+}
