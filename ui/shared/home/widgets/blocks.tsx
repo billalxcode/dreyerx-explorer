764d42fcbb9bde9f6edd5784cf93c17eeb633441
billalxcode <billal.xcode@gmail.com>
2024-12-31 07:42:10 +0700

    add table to blocks widget

--- ui/shared/home/widgets/blocks.tsx
+++ ui/shared/home/widgets/blocks.tsx
@@ -1,12 +1,28 @@
-import Card from '@/ui/components/card/card'
-import React from 'react'
+import Card from "@/ui/components/card/card";
+import TableContainer from "@/ui/components/table/container";
+import TableDataCol from "@/ui/components/table/Td";
+import TableHeaderCol from "@/ui/components/table/Th";
+import React from "react";
 
 export default function LatestBlocks() {
   return (
-    <Card title='Latest Blocks' className='w-1/4'>
-        <div className="flex flex-col w-full h-full justify-center items-center">
-            <p>No Data</p>
-        </div>
+    <Card title="Latest Blocks" className="w-1/3">
+      <TableContainer>
+        <thead>
+          <tr>
+            <TableHeaderCol>Block ID</TableHeaderCol>
+            <TableHeaderCol>Size</TableHeaderCol>
+            <TableHeaderCol>Time</TableHeaderCol>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <TableDataCol>1</TableDataCol>
+            <TableDataCol>20</TableDataCol>
+            <TableDataCol>1 minutes ago</TableDataCol>
+          </tr>
+        </tbody>
+      </TableContainer>
     </Card>
-  )
+  );
 }
