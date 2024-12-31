3be381ba172d31e1dc12bab07f2d50018f98eee8
billalxcode <billal.xcode@gmail.com>
2024-12-31 08:35:49 +0700

    add table data column

--- ui/components/table/Td.tsx
+++ ui/components/table/Td.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function TableDataCol(props: {
     children: React.ReactNode;
+    className?: string
 }) {
   return (
-    <td className="px-4 py-2 border border-container-default">{ props.children }</td>
+    <td className={`px-4 py-2 border border-container-default ${props.className}`}>{ props.children }</td>
   )
 }
