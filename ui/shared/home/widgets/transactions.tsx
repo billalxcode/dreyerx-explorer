a865b71fae2f3806e688ed0c27aa9727f754ea31
billalxcode <billal.xcode@gmail.com>
2024-12-31 07:42:18 +0700

    add card empty data

--- ui/shared/home/widgets/transactions.tsx
+++ ui/shared/home/widgets/transactions.tsx
@@ -1,12 +1,11 @@
 import Card from '@/ui/components/card/card'
+import CardEmptyData from '@/ui/components/card/empty'
 import React from 'react'
 
 export default function LatestTransactions() {
   return (
     <Card title='Latest Transactions' className='w-full'>
-        <div className="flex flex-col w-full h-full justify-center items-center">
-            <p>No Data</p>
-        </div>
+        <CardEmptyData />
     </Card>
   )
 }
