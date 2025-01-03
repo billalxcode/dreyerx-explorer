f995b1794c466b08b4fdd2236a73d3effc2b5ff2
billalxcode <billal.xcode@gmail.com>
2025-01-03 22:00:47 +0700

    add hash transaction details

---
+++ app/tx/[hash]/page.tsx
@@ -0,0 +1,20 @@
+import Container from '@/ui/components/container/container';
+import SearchBar from '@/ui/components/searchbar/searchbar';
+import Header from '@/ui/shared/header/header';
+import TransactionDetailsContainer from '@/ui/shared/transactions/details/container';
+import React from 'react';
+
+export default async function Page(props: { params: Promise<{ hash: string }> }) {
+    const transaction_hash = (await props.params).hash;
+    return (
+        <>
+            <Header />
+
+            <Container>
+                <SearchBar />
+
+                <TransactionDetailsContainer transaction={transaction_hash} />
+            </Container>
+        </>
+    );
+}
