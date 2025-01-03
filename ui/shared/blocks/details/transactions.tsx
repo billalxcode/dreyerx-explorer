b977fbd8a9ae572007febdea169b1836fbef5f1a
billalxcode <billal.xcode@gmail.com>
2025-01-03 22:04:32 +0700

    add shared ui block details transactions

---
+++ ui/shared/blocks/details/transactions.tsx
@@ -0,0 +1,34 @@
+"use client"
+import useBlockTransactions from '@/hooks/blocks/useBlockTransactions';
+import Card from '@/ui/components/card/card';
+import React, { useEffect } from 'react';
+import TransactionItem from '../../transactions/item';
+import CardEmptyData from '@/ui/components/card/empty';
+
+export default function BlockDetailsTransactions(props: { block: string }) {
+    const { handleFetchBlockTransactions, transactions } = useBlockTransactions(props.block)
+
+    useEffect(() => {
+        handleFetchBlockTransactions()
+    }, [
+        handleFetchBlockTransactions
+    ])
+    return <Card title="Transactions" className="w-full">
+        <div className="flex flex-col gap-2 mt-2 w-full h-full">
+            {
+            transactions.length === 0 ? (
+                <CardEmptyData className='mb-16' />
+            ) : (
+                transactions?.map((transaction) => {
+                    return (
+                        <TransactionItem
+                            transaction={transaction}
+                            key={transaction.hash}
+                        />
+                    );
+                })
+            )
+            }
+        </div>
+    </Card>;
+}
