bc82c2adc7fb03b1160ff928cffc63df392f6d04
billalxcode <billal.xcode@gmail.com>
2025-01-03 22:02:53 +0700

    add custom className at card empty data

--- ui/components/card/empty.tsx
+++ ui/components/card/empty.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-export default function CardEmptyData() {
+export default function CardEmptyData(props: {
+    className?: string;
+}) {
     return (
-        <div className="flex flex-col w-full h-full justify-center items-center">
+        <div className={`flex flex-col w-full h-full justify-center items-center ${props.className}`}>
             <p>No Data</p>
         </div>
     );
