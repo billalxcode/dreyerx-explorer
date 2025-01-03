c40937503f07b081a1e488d9b12e18cea87e1e96
billalxcode <billal.xcode@gmail.com>
2025-01-03 22:03:38 +0700

    fix motion library at searchbar component

--- ui/components/searchbar/searchbar.tsx
+++ ui/components/searchbar/searchbar.tsx
@@ -1,5 +1,6 @@
+"use client"
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion } from 'motion/react';
 import { CiSearch } from 'react-icons/ci';
 
 export default function SearchBar() {
