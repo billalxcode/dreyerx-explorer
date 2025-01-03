e7664e7a949d27ad2506e0e72cd6d01c3fb69ba8
billalxcode <billal.xcode@gmail.com>
2025-01-03 22:02:40 +0700

    fix redirect at card component

--- ui/components/card/card.tsx
+++ ui/components/card/card.tsx
@@ -1,5 +1,6 @@
-import Link from 'next/link';
-import React, { ReactNode } from 'react';
+"use client"
+import { useRouter } from 'next/navigation';
+import React, { ReactNode, useCallback } from 'react';
 
 export default function Card(props: {
     children?: React.ReactNode;
@@ -8,6 +9,13 @@ export default function Card(props: {
     className?: string;
     href?: string;
 }) {
+    const router = useRouter()
+    const onClick = useCallback((link: string) => {
+        router.replace(link)
+    }, [
+        router
+    ])
+
     const header = props.title ? (
         <div className="flex flex-row justify-between items-center">
             <h3 className="text-lg font-semibold">{props.title}</h3>
@@ -18,15 +26,12 @@ export default function Card(props: {
     const card = (
         <div
             className={`p-4 bg-container-default rounded-lg shadow-md border border-border-normal ${props.className}`}
+            onClick={() => props.href ? onClick(props.href) : null}
         >
             {header}
             {props.children}
         </div>
     );
-
-    if (props.href) {
-        return <Link href={props.href}>{card}</Link>;
-    }
 
     return card;
 }
