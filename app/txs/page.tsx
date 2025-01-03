393157b97e4bfbb2f9cf763593446966a6f0abd0
billalxcode <billal.xcode@gmail.com>
2025-01-03 22:00:57 +0700

    add page transactions list

---
+++ app/txs/page.tsx
@@ -0,0 +1,19 @@
+import Container from '@/ui/components/container/container'
+import SearchBar from '@/ui/components/searchbar/searchbar'
+import Header from '@/ui/shared/header/header'
+import TransactionsContainer from '@/ui/shared/transactions/container'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <>
+        <Header />
+
+        <Container>
+            <SearchBar />
+
+            <TransactionsContainer />
+        </Container>
+    </>
+  )
+}
