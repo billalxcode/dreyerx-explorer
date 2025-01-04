6d9f328f510da448a4ed9a8ce1ff9973c697e99c
billalxcode <billal.xcode@gmail.com>
2025-01-04 17:29:22 +0700

    add shared ui address container

---
+++ ui/shared/address/container.tsx
@@ -0,0 +1,14 @@
+import Card from '@/ui/components/card/card'
+import React from 'react'
+
+export default function AddressContainer(props: {
+    address: string
+}) {
+  return (
+    <Card title='Address Details'>
+        <Card className='mt-2'>
+            
+        </Card>
+    </Card>
+  )
+}
