0b5e3371d4d6a3947ad3bd444104ccd62e7c86fc
billalxcode <billal.xcode@gmail.com>
2025-01-04 12:14:49 +0700

    add transaction details data

---
+++ ui/shared/transactions/details/data.tsx
@@ -0,0 +1,50 @@
+import { Transaction } from '@/hooks/transactions/useTransactionDetails';
+import Card from '@/ui/components/card/card';
+import TableContainer from '@/ui/components/table/container';
+import TableDataCol from '@/ui/components/table/Td';
+import TableHeaderCol from '@/ui/components/table/Th';
+import React from 'react';
+
+export default function TransactionDetailsData(props: {
+    transaction: Transaction | null;
+}) {
+    return (
+        <Card title="Data">
+            <div className="flex flex-col gap-2 mt-2">
+                <div className="flex flex-col">
+                    <h2 className="text-white/50 text-sm">Call</h2>
+                    <p className="font-semibold text-lg">
+                        {props.transaction?.decoded_input.method_call ??
+                            'unknown'}
+                    </p>
+                </div>
+                <TableContainer>
+                    <thead>
+                        <tr>
+                            <TableHeaderCol className="w-16">#</TableHeaderCol>
+                            <TableHeaderCol>Name</TableHeaderCol>
+                            <TableHeaderCol>Type</TableHeaderCol>
+                            <TableHeaderCol>Value</TableHeaderCol>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.transaction?.decoded_input.parameters.map(
+                            (argument, index) => (
+                                <tr key={index}>
+                                    <TableDataCol className="text-center">
+                                        {index + 1}
+                                    </TableDataCol>
+                                    <TableDataCol>{argument.name}</TableDataCol>
+                                    <TableDataCol>{argument.type}</TableDataCol>
+                                    <TableDataCol>
+                                        {argument.value}
+                                    </TableDataCol>
+                                </tr>
+                            ),
+                        )}
+                    </tbody>
+                </TableContainer>
+            </div>
+        </Card>
+    );
+}
