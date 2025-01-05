e7980133e897c000d291a68d2a7d2d63d83783b6
billalxcode <billal.xcode@gmail.com>
2025-01-05 21:11:56 +0700

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Container from '@/ui/components/container/container';
 import SearchBar from '@/ui/components/searchbar/searchbar';
+import Footer from '@/ui/shared/footer/footer';
 import Header from '@/ui/shared/header/header';
 import HeroSection from '@/ui/shared/home/hero';
 import HomeWrapper from '@/ui/shared/home/wrapper';
@@ -17,6 +18,8 @@ export default function Home() {
 
                 <HomeWrapper />
             </Container>
+
+            <Footer />
         </>
     );
 }
