b2f639cfd9be6530f52a6012ffd046b252d11e72
billalxcode <billal.xcode@gmail.com>
2025-01-05 21:12:17 +0700

    add footer shared ui

--- ui/shared/footer/footer.tsx
+++ ui/shared/footer/footer.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <div className="flex justify-center items-center w-full py-10">
+            <p className="text-sm text-gray-400">© DreyerX Labs 2025, All rights reserved</p>    
+        </div>
+    );
 }
