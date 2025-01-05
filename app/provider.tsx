9b239167561d4fdee4eec52be6ac23e98132588d
billalxcode <billal.xcode@gmail.com>
2025-01-05 17:58:50 +0700

    fix mobile ui

--- app/provider.tsx
+++ app/provider.tsx
@@ -28,10 +28,10 @@ export default function Provider(props: { children: ReactNode }) {
         >
             <div
                 onMouseMove={handleMouseMove}
-                className="min-h-screen w-screen absolute inset-0 h-full bg-[radial-gradient(#ffffff2e,transparent_1px)] [background-size:16px_16px]"
+                className="min-h-screen w-full lg:w-screen md:w-full absolute inset-0 h-full bg-[radial-gradient(#ffffff2e,transparent_1px)] [background-size:16px_16px]"
             >
                 <div
-                    className="absolute w-[80px] h-[120px] bg-effect-cursor-default blur-[50px] z-[1] pointer-events-none"
+                    className="hidden lg:absolute w-[80px] h-[120px] bg-effect-cursor-default blur-[50px] z-[1] pointer-events-none"
                     style={{
                         left: `${glowPosition.x - 40}px`,
                         top: `${glowPosition.y - 60}px`,
