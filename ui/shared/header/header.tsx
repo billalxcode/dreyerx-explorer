635000678b7b9db8d7634b21c855155bf30ce860
billalxcode <billal.xcode@gmail.com>
2025-01-05 18:08:05 +0700

    fix mobile responsive

--- ui/shared/header/header.tsx
+++ ui/shared/header/header.tsx
@@ -1,13 +1,47 @@
+"use client"
+import Link from 'next/link';
 import HeaderMenu from './menu';
-import HeaderNetworkSelect from './network';
+// import HeaderNetworkSelect from './network';
+import { FaBars } from 'react-icons/fa6';
+import { useState } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+    const drawerVariants = {
+        hidden: { y: '100%' },
+        visible: { y: 0 },
+    };
+    
     return (
         <div className="flex justify-between items-center w-full p-4 select-none px-8">
-            <p className="font-bold">DREYERX EXPLORER</p>
-
-            <HeaderMenu />
-            <HeaderNetworkSelect />
+            <Link href={"/"} className="font-bold">DREYERX EXPLORER</Link>
+    
+            <div className="lg:hidden">
+                <FaBars onClick={toggleMenu} className="cursor-pointer" />
+            </div>
+    
+            <motion.div
+                className={`fixed inset-0 bg-black bg-opacity-70 backdrop-blur-2xl z-50 transition-transform lg:hidden`}
+                initial="hidden"
+                animate={isOpen ? "visible" : "hidden"}
+                variants={drawerVariants}
+            >
+                <div className="absolute w-full p-4">
+                    <div className="flex flex-col items-start h-full p-4">
+                        <div className="flex justify-end w-full">
+                            <FaTimes onClick={toggleMenu} className="cursor-pointer" />
+                        </div>
+                        <HeaderMenu />
+                        {/* <HeaderNetworkSelect /> */}
+                    </div>
+                </div>
+            </motion.div>
         </div>
     );
 }
--- ui/shared/header/menu.tsx
+++ ui/shared/header/menu.tsx
@@ -4,11 +4,11 @@ import React from 'react';
 
 export default function HeaderMenu() {
     return (
-        <ul className="flex flex-row gap-10">
+        <ul className="flex flex-col lg:flex-row gap-2 lg:gap-10 w-full mt-10 divide-y divide-container-default">
             {navigations.map((nav, index) => {
                 return (
                     <li
-                        className="cursor-pointer transition duration-300 hover:text-white"
+                        className="cursor-pointer transition duration-300 p-4"
                         key={index}
                     >
                         <Link href={nav.link} target={''}>
