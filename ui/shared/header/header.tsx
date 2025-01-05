0773a1378bdd1f8c56f93d0cd816b6b6cd0a146d
billalxcode <billal.xcode@gmail.com>
2025-01-05 19:58:09 +0700

    fix header responsive

--- ui/shared/header/header.tsx
+++ ui/shared/header/header.tsx
@@ -1,7 +1,7 @@
-"use client"
+'use client';
 import Link from 'next/link';
 import HeaderMenu from './menu';
-// import HeaderNetworkSelect from './network';
+import HeaderNetworkSelect from './network';
 import { FaBars } from 'react-icons/fa6';
 import { useState } from 'react';
 import { FaTimes } from 'react-icons/fa';
@@ -17,25 +17,35 @@ export default function Header() {
         hidden: { y: '100%' },
         visible: { y: 0 },
     };
-    
+
     return (
         <div className="flex justify-between items-center w-full p-4 select-none px-8">
-            <Link href={"/"} className="font-bold">DREYERX EXPLORER</Link>
-    
+            <Link href={'/'} className="font-bold">
+                DREYERX EXPLORER
+            </Link>
+
             <div className="lg:hidden">
                 <FaBars onClick={toggleMenu} className="cursor-pointer" />
             </div>
-    
+
+            <div className="hidden lg:gap-20 lg:flex lg:justify-between lg:items-center">
+                <HeaderMenu />
+                <HeaderNetworkSelect />
+            </div>
+
             <motion.div
                 className={`fixed inset-0 bg-black bg-opacity-70 backdrop-blur-2xl z-50 transition-transform lg:hidden`}
                 initial="hidden"
-                animate={isOpen ? "visible" : "hidden"}
+                animate={isOpen ? 'visible' : 'hidden'}
                 variants={drawerVariants}
             >
                 <div className="absolute w-full p-4">
                     <div className="flex flex-col items-start h-full p-4">
                         <div className="flex justify-end w-full">
-                            <FaTimes onClick={toggleMenu} className="cursor-pointer" />
+                            <FaTimes
+                                onClick={toggleMenu}
+                                className="cursor-pointer"
+                            />
                         </div>
                         <HeaderMenu />
                         {/* <HeaderNetworkSelect /> */}
