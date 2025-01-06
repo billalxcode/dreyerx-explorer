2b06967fc33d48514f575a91f22ecc7061748a09
billalxcode <billal.xcode@gmail.com>
2025-01-06 09:54:49 +0700

    add status to network types

--- config/constants.ts
+++ config/constants.ts
@@ -2,6 +2,7 @@ export type ExplorerNetworkTypes = {
     name: string;
     baseURL: string;
     apiURL: string;
+    status: "online" | "offline";
 };
 
 export const explorer_networks: ExplorerNetworkTypes[] = [
@@ -9,10 +10,12 @@ export const explorer_networks: ExplorerNetworkTypes[] = [
         name: 'Mainnet',
         baseURL: 'http://localhost:3000',
         apiURL: 'https://scan.dreyerx.com/api',
+        status: "online"
     },
     {
         name: 'Testnet',
         baseURL: 'http://localhost:3001',
         apiURL: 'https://testnet-scan.dreyerx.com/api',
+        status: "offline"
     },
 ];
