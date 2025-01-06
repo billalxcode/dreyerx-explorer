9ea71a5211d4cbefc3c2611a7dcf1e1e921898ae
billalxcode <billal.xcode@gmail.com>
2025-01-06 09:25:35 +0700

    fix lenis

--- app/provider.tsx
+++ app/provider.tsx
@@ -31,7 +31,7 @@ export default function Provider(props: { children: ReactNode }) {
 
     return (
         <ReactLenis
-            className="h-auto"
+            className="min-h-screen w-full"
             root
             ref={lenisRef}
             options={{
@@ -43,10 +43,10 @@ export default function Provider(props: { children: ReactNode }) {
         >
             <div
                 onMouseMove={handleMouseMove}
-                className="min-h-screen w-full lg:w-screen md:w-full absolute inset-0 h-full bg-[radial-gradient(#ffffff2e,transparent_1px)] [background-size:16px_16px]"
+                className="min-h-screen w-full lg:w-screen md:w-full inset-0 h-full bg-[radial-gradient(#ffffff2e,transparent_1px)] [background-size:16px_16px]"
             >
                 <div
-                    className="hidden lg:absolute w-[80px] h-[120px] bg-effect-cursor-default blur-[50px] z-[1] pointer-events-none"
+                    className="absolute sm:hidden w-[80px] h-[120px] bg-effect-cursor-default blur-[50px] z-[1] pointer-events-none"
                     style={{
                         left: `${glowPosition.x - 40}px`,
                         top: `${glowPosition.y - 60}px`,
