9a5ead81c76c2d23d8a89ff022f7f2ee8a91a951
billalxcode <billal.xcode@gmail.com>
2025-01-07 22:20:38 +0700

    add new hooks useAddress

---
+++ hooks/address/useAddress.tsx
@@ -0,0 +1,92 @@
+import { get_api_url } from '@/config/api';
+import { useState, useCallback } from 'react';
+
+export type Token = {
+    circulating_market_cap: string;
+    icon_url: string;
+    name: string;
+    decimals: string;
+    symbol: string;
+    address: string;
+    type: string;
+    holders: string;
+    exchange_rate: string;
+    total_supply: string;
+};
+
+export type PrivateTag = {
+    address_hash: string;
+    display_name: string;
+    label: string;
+};
+
+export type WatchlistName = {
+    display_name: string;
+    label: string;
+};
+
+export type PublicTag = {
+    address_hash: string;
+    display_name: string;
+    label: string;
+};
+
+export type AddressData = {
+    creator_address_hash: string;
+    creation_transaction_hash: string;
+    token: Token;
+    coin_balance: string;
+    exchange_rate: string;
+    implementation_address: string;
+    block_number_balance_updated_at: number;
+    hash: string;
+    implementation_name: string;
+    name: string;
+    is_contract: boolean;
+    private_tags: PrivateTag[];
+    watchlist_names: WatchlistName[];
+    public_tags: PublicTag[];
+    is_verified: boolean;
+    has_beacon_chain_withdrawals: boolean;
+    has_custom_methods_read: boolean;
+    has_custom_methods_write: boolean;
+    has_decompiled_code: boolean;
+    has_logs: boolean;
+    has_methods_read: boolean;
+    has_methods_write: boolean;
+    has_methods_read_proxy: boolean;
+    has_methods_write_proxy: boolean;
+    has_token_transfers: boolean;
+    has_tokens: boolean;
+    has_validated_blocks: boolean;
+};
+
+export default function useAddress(address: string) {
+    const [addressData, setAddressData] = useState<AddressData | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    
+    const handleFetchAddress = useCallback(async () => {
+        setIsLoading(true);
+        
+        const url = get_api_url(`/v2/addresses/${address}`);
+
+        fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then(async (response) => {
+            const data = await response.json();
+            setAddressData(data);
+            setIsLoading(false)
+        })
+    }, [
+        address,
+    ])
+
+    return {
+        addressData,
+        isLoading,
+        handleFetchAddress,
+    };
+}
