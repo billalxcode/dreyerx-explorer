a695dbac056d11f9690e3b85c0854276f1b6559c
billalxcode <billal.xcode@gmail.com>
2025-01-07 22:21:00 +0700

    implement address fetching and display in AddressContainer

--- ui/shared/address/container.tsx
+++ ui/shared/address/container.tsx
@@ -1,10 +1,28 @@
+"use client"
+import useAddress from '@/hooks/address/useAddress';
 import Card from '@/ui/components/card/card';
-import React from 'react';
+import React, { useEffect } from 'react';
+import AddressDetails from './details';
 
 export default function AddressContainer(props: { address: string }) {
+    const { address } = props;
+
+    const { addressData, handleFetchAddress } = useAddress(address)
+
+    useEffect(() => {
+        handleFetchAddress()
+    }, [
+        handleFetchAddress
+    ])
+
     return (
-        <Card title="Address Details">
-            <Card className="mt-2"></Card>
-        </Card>
+        <>
+            <Card>
+                <p className='text-yellow-600'>Feature under development</p>
+            </Card>
+            <Card title="Address Details">
+                <AddressDetails address={address} addressData={addressData} />
+            </Card>
+        </>
     );
 }
