5795d80500da21a38884a5f13b84af859ed34eae
billalxcode <billal.xcode@gmail.com>
2025-01-08 15:20:39 +0700

    fix: update asset URLs to use HTTPS for improved security and reliability

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,22 +21,22 @@ export const metadata: Metadata = {
     ],
     creator: 'DreyerX Labs',
     icons: {
-        apple: '/assets/apple-touch-icon.png',
-        icon: '/assets/favicon.ico',
+        apple: 'https://scan.dreyerx.com/assets/apple-touch-icon.png',
+        icon: 'https://scan.dreyerx.com/assets/favicon.ico',
     },
     robots: 'index, follow',
     openGraph: {
         title: 'DreyerX Explorer',
         description:
             'Explore the world of DreyerX with our comprehensive explorer.',
-        url: 'https://dreyerx.com',
+        url: 'https://scan.dreyerx.com',
         emails: ['info@dreyerx.com'],
         type: 'website',
         locale: 'en_US',
         siteName: 'DreyerX Explorer',
         images: [
             {
-                url: '/assets/og-image.png',
+                url: 'https://scan.dreyerx.com/assets/og-image.png',
                 width: 1920,
                 height: 1080,
                 alt: 'DreyerX Explorer',
@@ -50,7 +50,7 @@ export const metadata: Metadata = {
         title: 'DreyerX Explorer',
         description:
             'Explore the world of DreyerX with our comprehensive explorer.',
-        images: ['/assets/og-image.png'],
+        images: ['https://scan.dreyerx.com/assets/og-image.png'],
     },
 };
 
