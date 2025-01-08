72c640e4a441c2d7a52c60f85aebc0ad6c81bc83
billalxcode <billal.xcode@gmail.com>
2025-01-08 10:00:52 +0700

    feat: implement search functionality in SearchBar component with custom hook integration

--- ui/components/searchbar/searchbar.tsx
+++ ui/components/searchbar/searchbar.tsx
@@ -1,11 +1,30 @@
 'use client';
-import React from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import { motion } from 'motion/react';
 import { CiSearch } from 'react-icons/ci';
+import useSearch from '@/hooks/search/useSearch';
 
 export default function SearchBar() {
+    const [query, setQuery] = useState<string>('');
+    const { searchData, handleFetchSearch } = useSearch()
+
+    const handleSearch = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        handleFetchSearch(query)
+    }
+    
+    useEffect(() => {
+        console.log(searchData)
+    }, [searchData])
+
     return (
-        <div className="flex w-full justify-center">
+        <form
+        onSubmit={(e) => handleSearch(e)}
+        className="flex w-full justify-center"
+        autoComplete='off'
+        autoCorrect='off'
+        >
             <motion.div
                 whileHover={{
                     borderColor: 'rgba(255, 255, 255, 0.2)',
@@ -17,18 +36,23 @@ export default function SearchBar() {
                     ease: 'easeInOut',
                     duration: 0.4,
                 }}
-                className="flex flex-row-reverse w-[500px] bg-container-default backdrop-blur-lg justify-between items-center bg-opacity-10 rounded-lg p-3 border border-border-normal"
+                className="flex flex-row-reverse w-full lg:w-[500px] bg-container-default backdrop-blur-lg justify-between items-center bg-opacity-10 rounded-lg p-3 border border-border-normal"
             >
                 <input
                     type="text"
                     name="query"
                     id="query"
-                    className="w-full bg-transparent focus:outline-none"
+                    onChange={(e) => setQuery(e.target.value)}
+                    value={query}
+                    className="w-full bg-transparent focus:outline-none text-white/50"
                     placeholder="Search txns, accounts, blocks, and more"
+                    autoComplete="off"
+                    autoCorrect="off"
+                    spellCheck="false"
                 />
 
-                <CiSearch size={'24px'} className="justify-self-center mx-2" />
+                <CiSearch size={'24px'} className="justify-self-center mx-1" />
             </motion.div>
-        </div>
+        </form>
     );
 }
