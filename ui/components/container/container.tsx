4b6ae440ed8add2698068941d37a4c17711f16d7
billalxcode <billal.xcode@gmail.com>
2025-01-08 10:50:59 +0700

    fix: adjust top padding in Container component for improved layout consistency

--- ui/components/container/container.tsx
+++ ui/components/container/container.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Container(props: { children: React.ReactNode }) {
     return (
-        <div className="min-h-screen w-screen lg:w-screen px-4 lg:px-8 pt-8 flex flex-col gap-4">
+        <div className="min-h-screen w-screen lg:w-screen px-4 lg:px-8 pt-2 lg:pt-8 flex flex-col gap-4">
             {props.children}
         </div>
     );
