a9a761e311d26b5cdb5631e11b5c4a088cd6dbfa
billalxcode <billal.xcode@gmail.com>
2025-01-08 11:20:49 +0700

    fix: update footer copyright format for consistency

--- ui/shared/footer/footer.tsx
+++ ui/shared/footer/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="flex justify-center items-center w-full py-5 lg:py-5 bg-container-default border-t border-border-normal">
             <p className="text-sm text-white/50">
-                © DreyerX Labs 2025, All rights reserved
+                © 2025 DreyerX Labs. All rights reserved.
             </p>
         </div>
     );
