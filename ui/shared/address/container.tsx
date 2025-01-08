ac4ca34289da9e39bb10af6d6863ef2473cae992
billalxcode <billal.xcode@gmail.com>
2025-01-08 23:19:14 +0700

    feat: integrate useAddressTabCounter in AddressContainer for fetching address tab counters

--- ui/shared/address/container.tsx
+++ ui/shared/address/container.tsx
@@ -3,15 +3,18 @@ import useAddress from '@/hooks/address/useAddress';
 import Card from '@/ui/components/card/card';
 import React, { useEffect } from 'react';
 import AddressDetails from './details';
+import useAddressTabCounter from '@/hooks/address/useAddressTabCounter';
 
 export default function AddressContainer(props: { address: string }) {
     const { address } = props;
 
     const { addressData, handleFetchAddress } = useAddress(address);
+    const { handleFetchCounter, addressTabsCounters } = useAddressTabCounter(address);
 
     useEffect(() => {
         handleFetchAddress();
-    }, [handleFetchAddress]);
+        handleFetchCounter();
+    }, [handleFetchAddress, handleFetchCounter]);
 
     return (
         <>
@@ -19,7 +22,11 @@ export default function AddressContainer(props: { address: string }) {
                 <p className="text-yellow-600">Feature under development</p>
             </Card>
             <Card title="Address Details">
-                <AddressDetails address={address} addressData={addressData} />
+                <AddressDetails
+                    address={address}
+                    addressData={addressData}
+                    addressTabsCounters={addressTabsCounters}
+                />
             </Card>
         </>
     );
