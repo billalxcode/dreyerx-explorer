3bc7a4ac0b40b4ff33898b0abc55a42f025b8582
billalxcode <billal.xcode@gmail.com>
2025-01-08 08:38:55 +0700

    fix: update Footer component styles for improved visibility and consistency

--- ui/shared/footer/footer.tsx
+++ ui/shared/footer/footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div className="flex justify-center items-center w-full py-5 lg:py-10">
-            <p className="text-sm text-gray-400">
+        <div className="flex justify-center items-center w-full py-5 lg:py-5 bg-container-default border-t border-border-normal">
+            <p className="text-sm text-white/50">
                 © DreyerX Labs 2025, All rights reserved
             </p>
         </div>
