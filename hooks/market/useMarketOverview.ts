9023936ada02d8298e9faf82ca96bda3c7dc899e
billalxcode <billal.xcode@gmail.com>
2025-01-08 17:10:18 +0700

    fix: add CORS headers to useMarketOverview hook for improved API accessibility

--- hooks/market/useMarketOverview.ts
+++ hooks/market/useMarketOverview.ts
@@ -124,6 +124,9 @@ export default function useMarketOverview() {
             }, {
                 headers: {
                     "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                    "Access-Control-Allow-Headers": "Content-Type, Authorization"
                 }
             });
             const data = response.data;
